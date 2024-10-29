553c7c5d97869a154f41cefab73b40117bff803f
ahmedieeeeee <139884613+ahmedieeeeee@users.noreply.github.com>
2024-10-29 12:31:15 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function validateLogin(event) {
+    event.preventDefault();
+    alert("Login form submitted successfully!");
+}
+
+function validateRegistration(event) {
+    event.preventDefault();
+    alert("Registration form submitted successfully!");
+}
